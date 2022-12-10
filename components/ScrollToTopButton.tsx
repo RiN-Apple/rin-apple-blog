0ef5bb7b1889a716b56c879926c339d4c9a2308c
RiN5-Apple <tridwn0ek@gmail.com>
2022-12-10 22:01:06 +0900

    [update]: スクロールボタンの大きさ変更

--- components/ScrollToTopButton.tsx
+++ components/ScrollToTopButton.tsx
@@ -10,7 +10,7 @@ const ScrollToTopButton: NextPage = () => {
   return (
     <div className='relative'>
       <button
-        className='fixed right-8 bottom-8 justify-center items-center w-16 h-16 font-notoserif text-6xl bg-blue-300 hover:bg-blue-200 rounded-full'
+        className='fixed right-8 bottom-8 justify-center items-center w-12 h-12 font-notoserif text-4xl bg-blue-300 hover:bg-blue-200 rounded-full'
         onClick={scrollToTop}
       >
         ^
