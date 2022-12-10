3fe66a04663a10fe059cd29dcd0ff5ca9823fffe
RiN5-Apple <tridwn0ek@gmail.com>
2022-12-10 22:33:35 +0900

    [update]: ヘッダーデザインを変更

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,13 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
-import NavLink from './header/NavLink';
-import NavDropDown from './header/NavDropDown';
-import NavDropDownMobile from './header/NavDropDownMobile';
 
 const Header: NextPage = () => {
   return (
-    <header className='flex flex-col pt-2 w-full font-notoserif text-gray-700  bg-white sm:flex-row sm:justify-between sm:py-3 sm:px-8 md:px-14'>
-      <h1 className='my-4 text-lg text-center sm:my-0'>
-        <Link href='/'>RiN Blog(仮)</Link>
+    <header className='flex flex-col w-full font-notoserif text-gray-700 bg-white'>
+      <h1 className='mt-3 mb-2 text-center'>
+        <Link href='/'>RiN5 BLOG</Link>
       </h1>
-      <nav className='hidden flex-row flex-nowrap justify-center text-sm sm:flex'>
-        <NavLink href='/' text='車' />
-        <NavLink href='/' text='プログラミング' />
-        <NavLink href='/' text='PC・ガジェット' />
-        <NavLink href='/' text='絵' />
-        <NavDropDown tags={['動画', '雑記', 'Home']} />
-      </nav>
-      <nav className='flex  justify-center bg-gray-800 sm:hidden'>
-        {/* <a className='py-1 w-full text-center text-white pointer-events-none'>カテゴリ &#9662;</a> */}
-        <NavDropDownMobile tags={[]} />
-      </nav>
+      <div className='w-full h-1 bg-gray-600' />
     </header>
   );
 };
