579c2f47b9dca8ec5ebe24b596b2d022d58341a3
RiN5-Apple <tridwn0ek@gmail.com>
2022-12-10 23:37:57 +0900

    [update]:トップページ(index)修正(途中)

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,21 @@ import Meta from '../components/Meta';
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='flex flex-col gap-y-5 p-5'>
       <Meta title='Home' description='RiNBlogのHomeページ。' />
-      <h1 className='pt-96'>Home</h1>
-      <p>検索</p>
-      <p>新着記事</p>
-      <p>人気ランキング</p>
-      <p>トップカテゴリ</p>
-      <p>人気カテゴリへのリンク</p>
-      <p>プロフィール</p>
+      <div>
+        <h2>プロフィール</h2>
+      </div>
+      <div>
+        <h2>カテゴリ</h2>
+        <p>検索</p>
+        <p>新着記事</p>
+        <p>人気ランキング</p>
+        <p>トップカテゴリ</p>
+        <p>人気カテゴリへのリンク</p>
+        <p>プロフィール</p>
+      </div>
+      <h2 className='py-96'>コンテンツ</h2>
     </div>
   );
 };
