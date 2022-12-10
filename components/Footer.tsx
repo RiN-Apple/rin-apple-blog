00beef53f79f236903c27fa5a1e043a755d02a5f
RiN5-Apple <tridwn0ek@gmail.com>
2022-12-10 22:42:03 +0900

    [update]: footerデザインを更新

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,13 @@
 import type { NextPage } from 'next';
-import FooterNavLink from './footer/FooterNavLink';
+import Link from 'next/link';
 
 const Footer: NextPage = () => {
   return (
-    <footer className='flex flex-col justify-center pt-2 mx-auto space-y-2 h-24 font-notoserif text-center text-gray-200 bg-gray-800'>
-      <h1>RiNブログ（仮）</h1>
-      <nav className='flex flex-row flex-nowrap justify-center text-xs'>
-        <FooterNavLink href='/' text='記事一覧' />
-        <FooterNavLink href='/about' text='プロフィール' last />
-      </nav>
-      <p className='text-xs'>&copy;2022 RiN Apple </p>
+    <footer className='flex flex-col justify-between p-3 mx-auto font-notoserif text-center text-gray-200 bg-gray-800'>
+      <p className='text-sm'>
+        <Link href='/'>RiN5 BLOG</Link>
+      </p>
+      <p className='text-xs'>&copy;2022- RiN5-Apple</p>
     </footer>
   );
 };
