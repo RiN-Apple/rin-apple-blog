a126c9a21b6aa6daa93f5d7b720e6240b807b435
RiN5Apple <tridwn0ek@gmail.com>
2022-12-12 01:15:26 +0900

    [update]: footer w-full

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Footer: NextPage = () => {
   return (
-    <footer className='flex flex-col justify-between p-3 mx-auto font-notoserif text-center text-gray-200 bg-gray-800'>
+    <footer className='flex flex-col w-full justify-between p-3 mx-auto font-notoserif text-center text-gray-200 bg-gray-800'>
       <p className='text-sm'>
         <Link href='/'>RiN5 BLOG</Link>
       </p>
