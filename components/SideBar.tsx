7071f5160aec0d7becafbd8d32468cd81aec7d4b
RiN5-Apple <tridwn0ek@gmail.com>
2023-01-03 01:28:28 +0900

    [update]: サイドバーデザイン修正

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -7,8 +7,9 @@ const SideBar: NextPage = () => {
     <aside className='w-full sm:w-1/4'>
       <div className='flex flex-col gap-y-5 sm:sticky sm:top-5'>
         <div className='w-full rounded-md bg-white p-2'>
-          <div className='rounded-md border-4 border-gray-500 bg-white p-3'>
-            <h2 className='mb-2 text-center text-lg font-semibold underline'>カテゴリ</h2>
+          <div className='rounded-md border-4 border-gray-400 bg-white p-3'>
+            <h2 className='text-center text-lg font-semibold'>カテゴリ</h2>
+            <div className='mb-2 h-0.5 w-full bg-gray-400'></div>
             <li>プログラミング</li>
             <li>車</li>
             <li>イラスト</li>
@@ -16,8 +17,9 @@ const SideBar: NextPage = () => {
           </div>
         </div>
         <div className='w-full rounded-md bg-white p-2'>
-          <div className='rounded-md border-4 border-gray-500 bg-white p-3'>
-            <h2 className='mb-2 text-center text-lg font-semibold underline'>このブログについて</h2>
+          <div className='rounded-md border-4 border-gray-400 bg-white p-3'>
+            <h2 className=' text-center text-lg font-semibold'>このブログについて</h2>
+            <div className='mb-2 h-0.5 w-full bg-gray-400'></div>
             <p>どうもRiN5です。趣味はドライブ、プログラミング、イラスト、音楽、片付けです</p>
             <button
               className=' h-8 w-8 items-center rounded-full bg-white text-center hover:bg-gray-200'
