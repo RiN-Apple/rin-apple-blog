2875c7a5c15d150db5472f5be7c98d5c2e668118
RiN5Apple <tridwn0ek@gmail.com>
2023-01-03 13:15:24 +0900

    [update]: meta 修正

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -4,9 +4,11 @@ import Head from 'next/head';
 type Props = {
   title: string;
   description: string;
+  url?: string;
+  img?: string;
 };
 
-const Meta: NextPage<Props> = ({ title, description }: Props) => {
+const Meta: NextPage<Props> = ({ title, description, url, img }: Props) => {
   return (
     <>
       <Head>
@@ -14,6 +16,12 @@ const Meta: NextPage<Props> = ({ title, description }: Props) => {
         <meta charSet='utf-8' />
         <meta content={description} name='description' />
         <meta name='viewport' content='width=device-width,initial-scale=1.0' />
+        <meta property='og:url' content={'https://www.rin5apple.com' + { url }} />
+        <meta property='og:type”' content='website' />
+        <meta property='og:title”' content={title} />
+        <meta property='og:description”' content={description} />
+        <meta property='og:site_name”' content='RiN5 BLOG' />
+        {img && <meta property='og:image' content={img} />}
       </Head>
     </>
   );
