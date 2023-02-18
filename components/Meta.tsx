d8709d677216485fa5f70a6dd23cc61ea7694bd3
RiN5apple <tridwn0ek@gmail.com>
2023-02-18 12:32:20 +0900

    [fix] 2/15の記事のメタタグを修正

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -12,9 +12,11 @@ const Meta: NextPage<Props> = ({ title, description, url, img }: Props) => {
   return (
     <>
       <Head>
-        <title>{`${title} | RiN5 BLOG`}</title>
         <meta charSet='utf-8' />
-        <meta content={description} name='description' />
+        <title>{`${title} | RiN5 BLOG`}</title>
+        <meta name='robots' content='index,follow' />
+        <meta name='googlebot' content='index,follow' />
+        <meta name='description' content={description} />
         <meta name='viewport' content='width=device-width,initial-scale=1.0' />
         <meta property='og:url' content={'https://blog.rin-five.com' + { url }} />
         <meta property='og:type”' content='website' />
